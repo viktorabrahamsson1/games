26fcd281986ff8251f0a4c7b522f5ba4010b63ca
Viktor <vikabr0524@gmail.com>
2024-11-30 16:36:02 +0100

    flappy bird done

--- FlappyBird/src/App.java
+++ FlappyBird/src/App.java
@@ -8,7 +8,6 @@ public class App {
         JFrame frame = new JFrame("Flappy Bird");
         // frame.setVisible(true);
         frame.setSize(boardWidth, boardHeight);
-
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
